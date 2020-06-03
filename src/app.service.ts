03b417cf2a2892edd9ebca6a4b25a34924e2381a
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-06-03 10:34:44 +0200

    port update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,17 @@
 import { Injectable, HttpService } from '@nestjs/common';
-<<<<<<< HEAD
-
+import * as Circuitbreaker from 'opossum';
 const urlMonitor = 'http://localhost:3400/';
 const urlServiceB = 'http://localhost:3000/';
-=======
-import * as Circuitbreaker from 'opossum'
-const urlMonitor = 'http://localhost:3000/'
-const urlServiceB = 'http://localhost:3002/'
->>>>>>> a4a0d9c22593891e4d654a5f9a9cf7a953cc1fe7
 
 const options = {
-  timeout:3000,
+  timeout: 3000,
   resetTimeout: 10000,
-}
-const circuitBreakerError = JSON.parse('{"data": "CircuitBreaker open", "level": "circuitbreakerError"}')
+};
+const circuitBreakerError = JSON.parse(
+  '{"data": "CircuitBreaker open", "level": "circuitbreakerError"}',
+);
 @Injectable()
 export class AppService {
-
   constructor(private httpService: HttpService) {}
 
   createErrorMessage(data: string) {
@@ -28,12 +23,11 @@ export class AppService {
   async sendError(data: JSON) {
     const send = await this.httpService.post(urlMonitor, data).toPromise();
   }
-   async handleRequestToB() {
-    const breaker = new Circuitbreaker(this.sendToB(), options)
-    const response = await breaker.fire()
-    .catch(console.error)
-    console.log('After catch')
-    breaker.fallback((response) => this.sendError(response.data))
+  async handleRequestToB() {
+    const breaker = new Circuitbreaker(this.sendToB(), options);
+    const response = await breaker.fire().catch(console.error);
+    console.log('After catch');
+    breaker.fallback(response => this.sendError(response.data));
   }
   async sendToB() {
     try {
@@ -42,12 +36,7 @@ export class AppService {
         console.log('Request to B was successful');
       }
     } catch (error) {
-<<<<<<< HEAD
-      this.sendError(error.response.data);
-=======
-      
-      return Promise.reject(error.response.data)
->>>>>>> a4a0d9c22593891e4d654a5f9a9cf7a953cc1fe7
+      return Promise.reject(error.response.data);
     }
   }
 }
