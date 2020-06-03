d34810e51b70187677a3f814191e19f3b2cd521a
trod <st104680@stud.uni-stuttgart.de>
2020-06-03 09:45:36 +0200

    Added a CircuitBreaker with the cockatiel library

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, HttpService } from '@nestjs/common';
+import { Policy, ConsecutiveBreaker, BrokenCircuitError} from 'cockatiel'
 
 const urlMonitor = 'http://localhost:3000/'
 const urlServiceB = 'http://localhost:3002/'
+const circuitBreaker = Policy.handleAll().circuitBreaker(10*1000, new ConsecutiveBreaker(3));
 
 @Injectable()
 export class AppService {
@@ -13,18 +15,33 @@ export class AppService {
     return JSON.parse(string)
   }
 
+  async handleRequest() {
+    try {
+      const data = await circuitBreaker.execute(() => this.sendToB())  
+    } catch (error) {
+      if (error instanceof BrokenCircuitError ) {
+        console.log('Breaker open')
+        this.sendError(JSON.parse('{ "data": "CircuitBreaker open", "level": "CircuitBreakerError"}'))
+      } else {
+        console.log(error)
+        this.sendError(error)
+      }
+    }
+  }
+
   async sendError(data: JSON) {
     const send = await this.httpService.post(urlMonitor, data).toPromise();
   }
 
   async sendToB() {
+    console.log('B called')
     try {
       const send = await this.httpService.get(urlServiceB).toPromise();
       if (send.status == 200) {
         console.log('Request to B was successful')
       }
     } catch (error) {
-      this.sendError(error.response.data)
+      return Promise.reject(error.response.data)
     }
   }
 
