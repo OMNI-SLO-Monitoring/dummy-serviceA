490238c63055bf84cc8d08f48002ecca433a56fe
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-06-03 11:55:24 +0200

    port updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,30 +1,37 @@
 import { Injectable, HttpService } from '@nestjs/common';
-import { Policy, ConsecutiveBreaker, BrokenCircuitError} from 'cockatiel'
+import { Policy, ConsecutiveBreaker, BrokenCircuitError } from 'cockatiel';
 
-const urlMonitor = 'http://localhost:3000/'
-const urlServiceB = 'http://localhost:3002/'
-const circuitBreaker = Policy.handleAll().circuitBreaker(10*1000, new ConsecutiveBreaker(3));
+const urlMonitor = 'http://localhost:3400/';
+const urlServiceB = 'http://localhost:3000/';
+const circuitBreaker = Policy.handleAll().circuitBreaker(
+  10 * 1000,
+  new ConsecutiveBreaker(3),
+);
 
 @Injectable()
 export class AppService {
   constructor(private httpService: HttpService) {}
-  
+
   createErrorMessage(data: string) {
-    let string = '{ "data": ' + JSON.stringify(data) + ', "level": "error"}'
-    console.log(string)
-    return JSON.parse(string)
+    let string = '{ "data": ' + JSON.stringify(data) + ', "level": "error"}';
+    console.log(string);
+    return JSON.parse(string);
   }
 
   async handleRequest() {
     try {
-      const data = await circuitBreaker.execute(() => this.sendToB())  
+      const data = await circuitBreaker.execute(() => this.sendToB());
     } catch (error) {
-      if (error instanceof BrokenCircuitError ) {
-        console.log('Breaker open')
-        this.sendError(JSON.parse('{ "data": "CircuitBreaker open", "level": "CircuitBreakerError"}'))
+      if (error instanceof BrokenCircuitError) {
+        console.log('Breaker open');
+        this.sendError(
+          JSON.parse(
+            '{ "data": "CircuitBreaker open", "level": "CircuitBreakerError"}',
+          ),
+        );
       } else {
-        console.log(error)
-        this.sendError(error)
+        console.log(error);
+        this.sendError(error);
       }
     }
   }
@@ -34,15 +41,14 @@ export class AppService {
   }
 
   async sendToB() {
-    console.log('B called')
+    console.log('B called');
     try {
       const send = await this.httpService.get(urlServiceB).toPromise();
       if (send.status == 200) {
-        console.log('Request to B was successful')
+        console.log('Request to B was successful');
       }
     } catch (error) {
-      return Promise.reject(error.response.data)
+      return Promise.reject(error.response.data);
     }
   }
-
 }
